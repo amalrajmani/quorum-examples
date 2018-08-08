c8de51514457f7547edeb5480e1b7855c1db5696
amalraj.manigmail.com <amalraj.manigmail.com>
2018-08-08 15:36:30 +0800

    add sleep to give time for block creation

--- nodejs/test/storage-root.sh
+++ nodejs/test/storage-root.sh
@@ -2,7 +2,7 @@
 
 ca=$2
 nid=$1
-
+sleep 1
 x=$(geth attach ipc:/home/vagrant/quorum-examples/examples/7nodes/qdata/dd$nid/geth.ipc <<EOF
 var b=eth.blockNumber;
 var sr=eth.storageRoot(eth.getTransactionReceipt("$ca").contractAddress);
